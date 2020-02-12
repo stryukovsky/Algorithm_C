00314e872c096bdbe8f57d88923a7fb29101c2c5
dmitry <strukovsky1@gmail.com>
2020-02-12 08:02:37 +0300

    Add binary search or insert

--- search/binary_search.c
+++ search/binary_search.c
@@ -21,3 +21,30 @@ int binary_search(const dtype* arr, int val, int n)
     }
     return -1;
 }
+
+// Alternate version of algorithm - "search or insert)
+
+int bs_contains(const dtype* arr, int val, int n)
+{
+    int low = 0;
+    int high = n-1;
+    while(low<=high)
+    {
+        int middle = (low+high)/2;
+        dtype pivot = arr[middle];
+        if(pivot > val)
+        high = middle-1;
+        else if(pivot < val)
+            low = middle+1;
+        else return middle;
+    }
+
+    return -(low+1);
+}
+
+int binary_search_insert(dtype* arr, int val, int n)
+{
+    int pos = bs_contains(arr, val, n);
+    if(pos<0)
+        arr[-pos+1] = val;
+}
